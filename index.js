9aef7a09d4364c0b53d29608e80bd22bf9ed6761
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-17 01:09:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ main.remove();
 /* Create your new element here and assign it to newHeader */
 
 
-var newHeader = document.createElement('victory');
+const newHeader = document.createElement('h1');
 const newHeader.innerText = "i'm the best";
+newHeader.id = victory
