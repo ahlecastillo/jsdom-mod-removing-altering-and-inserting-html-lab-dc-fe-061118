f593aec6a53024f9a984ea110fbb227f3b39bf63
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-17 00:56:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ main.remove();
 /* Create your new element here and assign it to newHeader */
 
 document.createElement('newHeader');
+
 newHeader.innerHTML("I'm the best"); 
 document.body.appendChild('h1'); 
